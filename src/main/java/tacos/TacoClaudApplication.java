2eb41d49441707413c98f5d13a27bd686f61b9d5
lugovskyi <lvcom64@gmail.com>
2022-09-24 01:01:36 +0300

    init commit

---
+++ src/main/java/tacos/TacoClaudApplication.java
@@ -0,0 +1,13 @@
+package tacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoClaudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoClaudApplication.class, args);
+    }
+
+}
